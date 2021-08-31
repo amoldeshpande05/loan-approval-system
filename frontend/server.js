9244245c293910d252ef5d49f6de2475567d14e7
amoldeshpande05 <amoldeshpande05>
2021-08-31 16:08:04 +0530

    remove

--- frontend/server.js
+++ frontend/server.js
@@ -7,26 +7,6 @@ app.get('/', (req, res) => {
   res.sendFile(jsonPath);
 })
 
-app.get('/validateLoan', (req, res) => {
-
-  let email = req.query.email;
-  let fname = req.query.fname;
-  let lname = req.query.lname;
-  let loan = req.query.loan;
-  let sourceIncome = req.query.sourceIncome;
-  // let age = document.getElementById('age').value;
-  let income = req.query.income;
-  let reason = req.query.reason;
-
-
-  if( parseInt(income)*5 >= parseInt(loan)){
-    res.json({"result":"Loan Approved",status:200})
-  }else{
-    res.json({"result":"Invalid data",status:500})
-  }
-})
-
-
 app.listen(port, (req,res) => {
   console.log("Listening on the port  : ",port)
 })
